95e4bb5110220f5ce01e51f0953a45b04abbaf72
Leonardo Adams <leonardoadams@gmail.com>
2019-08-28 19:02:37 -0300

    Create redux store and apply to the app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import middleware from './middleware'
 
+/**
+* @description Connect the app to the html main page. Creates the redux store and connect the app to it.
+*/
+
+const store = createStore(reducer, middleware)
+    
 ReactDOM.render(
-    <App />, 
-    document.getElementById('root'))
-
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+)
