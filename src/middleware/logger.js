a457f33a4d63647827b52ee3db5130e267f86bc6
Leonardo Adams <leonardoadams@gmail.com>
2019-08-28 18:36:38 -0300

    Logger Middleware

---
+++ src/middleware/logger.js
@@ -0,0 +1,13 @@
+/**
+* @description Log store changes
+*/
+const logger = (store) => (next) => (action) => {
+	console.group(action.type)
+  		console.log('The action :', action)
+  		const returnValue = next(action)
+        console.log('The new state: ', store.getState())
+  	console.groupEnd()
+  	return returnValue
+}
+
+export default logger
