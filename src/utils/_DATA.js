191a59fc747c560fe732615c400359447ded5299
Leonardo Adams <leonardoadams@gmail.com>
2019-09-01 01:04:10 -0300

    Fix avatar url

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
     sarahedo: {
       id: 'sarahedo',
       name: 'Sarah Edo',
-      avatarURL: "https://localhost:3000/src/icons/person.svg",
+      avatarURL: "https://tylermcginnis.com/would-you-rather/sarah.jpg",
       answers: {
         "8xf0y6ziyjabvozdd253nd": 'optionOne',
         "6ni6ok3ym7mf1p33lnez": 'optionOne',
@@ -14,7 +14,7 @@ let users = {
     tylermcginnis: {
       id: 'tylermcginnis',
       name: 'Tyler McGinnis',
-      avatarURL: "./icons/person.svg",
+      avatarURL: "https://tylermcginnis.com/would-you-rather/tyler.jpg",
       answers: {
         "vthrdm985a262al8qx3do": 'optionOne',
         "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
     johndoe: {
       id: 'johndoe',
       name: 'John Doe',
-      avatarURL: "./icons/person.svg",
+      avatarURL: "/images/person.svg",
       answers: {
         "xj352vofupe1dqz9emx13r": 'optionOne',
         "vthrdm985a262al8qx3do": 'optionTwo',
