20fa83450dc9a106a3b279104c5259cf6627f38f
Leonardo Adams <leonardoadams@gmail.com>
2019-09-01 22:53:52 -0300

    Routes configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { connect } from 'react-redux'
 import LoadingBar from 'react-redux-loading'
 import { handleInitialData } from '../actions/shared'
 import Nav from './Nav'
 import LogBar from './LogBar'
+import PrivateRootRoute from './PrivateRootRoute'
+import PrivateRoute from './PrivateRoute'
+import PollsPage from './PollsPage'
+import LoginPage from './LoginPage'
+import PollVotePage from './PollVotePage'
+import PollResultsPage from './PollResultsPage'
+import NewQuestionPage from './NewQuestionPage'
+import LeaderBoardPage from './LeaderBoardPage'
+import NotFoundPage from './NotFoundPage'
 
 
 class App extends Component {
@@ -21,6 +30,19 @@ class App extends Component {
             {this.props.logged === true && (
               <LogBar />
             )}
+          </div>
+          <div>
+            <Switch>
+              <PrivateRootRoute path='/' exact component={PollsPage} />
+              <PrivateRoute path='/polls' component={PollsPage} />
+              <PrivateRoute path='/question/:id' exact component={PollVotePage} />
+              <PrivateRoute path='/question/:id/results' exact component={PollResultsPage} />
+              <Route path='/login' exact component={LoginPage} />
+              <PrivateRoute path='/add' exact component={NewQuestionPage} />
+              <PrivateRoute path='/leaderboard' exact component={LeaderBoardPage} />
+              <Route path='/pagenotfound' exact component={NotFoundPage} />
+              <Route component={NotFoundPage} />
+            </Switch>
           </div>
         </div>
       </BrowserRouter>
